7f9c5f2934adeedc4cbfb48e40838e1b3b3d274e
randy hac <randydhacks@gmail.com>
2024-01-03 19:44:23 -0800

    completed play.js file

--- commands/play.js
+++ commands/play.js
@@ -38,5 +38,86 @@ module.exports = {
                         .setDescription('search keywords')
                         .setRequired(true)
                 })
+        }),
+    execute: async ({client, interaction}) => {
+        if (!interaction.member.voice.channel) {
+            await interaction.reply("You must be in voice channel to use this command.")
+            return;
+        }
+
+        const queue = await client.player.createQueue(interaction.guild);
+
+        if (!queue.connection) await queue.connect(interaction.member.voice.channel)
+
+        let embed = new MessageEmbed();
+
+        if (interaction.options.getSubcommand() === 'song') {
+            let url = ineraction.options.getString('url')
+
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.YOUTUBE_VIDEO,
+            })
+
+            if (result.tracks.length === 0) {
+                await interaction.reply("no results found");
+                return;
+            }
+
+            const song = result.tracks[0];
+            await queue.addTrack(song);
+
+            embed
+                .setDescription(`Added **[${song.title}](${song.url})** to the queue.`)
+                .setThumbnail(song.thumbnail)
+                .setFooter({text: `Duration: ${song.duration}`})
+
+        } else if (interaction.options.getSubcommand() === 'playlist') {
+            let url = ineraction.options.getString('url')
+
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.YOUTUBE_PLAYLIST,
+            })
+
+            if (result.tracks.length === 0) {
+                await interaction.reply("no playlist found");
+                return;
+            }
+
+            const playlist = result.playlist;
+            await queue.addTrack(playlist);
+
+            embed
+                .setDescription(`Added **[${playlist.title}](${playlist.url})** to the queue.`)
+                .setThumbnail(playlist.thumbnail)
+                .setFooter({text: `Duration: ${playlist.duration}`})
+        } else if (interaction.options.getSubcommand() === 'searchterms') {
+            let url = ineraction.options.getString('url')
+
+            const result = await client.player.search(url, {
+                requestedBy: interaction.user,
+                searchEngine: QueryType.YOUTUBE_AUTO,
+            })
+
+            if (result.tracks.length === 0) {
+                await interaction.reply("no results found");
+                return;
+            }
+
+            const song = result.tracks[0];
+            await queue.addTrack(song);
+
+            embed
+                .setDescription(`Added **[${song.title}](${song.url})** to the queue.`)
+                .setThumbnail(song.thumbnail)
+                .setFooter({text: `Duration: ${song.duration}`})
+        }
+
+        if (!queue.playing) await queue.play();
+
+        await interaction.reply({
+            embeds: [embed]
         })
+    }
 }
